480e0ec5bf42af8fbb37603257707832eb6e73e4
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-07-18 21:40:27 +0300

    Delete old , and new iter_tokens.py

--- iter_tokens.py
+++ iter_tokens.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-import re
+from typing import Iterator
 from enum import Enum
+import re
 
 
 TOKEN_SPECS = {
-    "NUM": r"[A-Za-z_]\w*",
-    "PLUS": r"\+",
-    "MINUS": "-",
-    "MUL": r"\*",
-    "DIV": r"/",
-    "WS": r"\s+",
+    name: rf"(?P<{name}>{pat})"
+    for name, pat in {
+        "PLUS": r"\+",
+        "MINUS": r"-",
+        "MUL": r"\*",
+        "DIV": r"/",
+        "LPAREN": r"\(",
+        "RPAREN": r"\)",
+        "NAME": r"[A-Za-z_]\w*",
+        "NUM": r"\d+",
+        "WS": r"\s+",
+    }.items()
 }
-
 Sym = Enum("Sym", TOKEN_SPECS)
 
 
 class Token:
-    def __init__(self, name: Sym, pat: str, val: str | float):
-        pass
+    def __init__(self, name: str, val: float | str):
+        self.name = name
+        self.val = val
+
+    def __repr__(self):
+        a = ", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())
+        return f"{type(self).__name__}({a})"
 
 
-tokens = {name: Token(Sym[name], pat, pat) for name, pat in TOKEN_SPECS.items()}
+def iter_tokes(sexpr: str) -> Iterator[Token]:
+    master_pat = "|".join(pat for _, pat in TOKEN_SPECS.items())
+    for m in re.finditer(master_pat, sexpr):
+        if m.lastgroup == Sym.WS.name:
+            continue
+        yield Token(m.lastgroup, val=m.group(0))
+
+
+if __name__ == "__main__":
+    for tok in iter_tokes("2 + (3 * 4) + 5"):
+        print(tok)
