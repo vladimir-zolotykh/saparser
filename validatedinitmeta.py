07c44f6f1d882b244d80f58c85509abb079522b5
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-07-21 09:09:52 +0300

    Shorter names: ns, name, one long if
            if ns.get("_DO_INIT"):
                super_init = ns["__init__"]

--- validatedinitmeta.py
+++ validatedinitmeta.py
@@ -15,30 +15,29 @@ def check_type(value: Any, hint: Any) -> bool:
 
 
 class ValidatedInitMeta(type):
-    def __new__(mcls, clsname, bases, clsdict):
-        if not clsdict.get("_DO_INIT"):
-            return super().__new__(mcls, clsname, bases, clsdict)
-        super_init = clsdict["__init__"]
+    def __new__(mcls, name, bases, ns):
+        if ns.get("_DO_INIT"):
+            super_init = ns["__init__"]
 
-        sig = inspect.signature(super_init)
-        hints = get_type_hints(super_init)
+            sig = inspect.signature(super_init)
+            hints = get_type_hints(super_init)
 
-        def new_init(*args, **kwargs):
-            bound = sig.bind(*args, **kwargs)
-            bound.apply_defaults()
+            def new_init(*args, **kwargs):
+                bound = sig.bind(*args, **kwargs)
+                bound.apply_defaults()
 
-            for name, value in bound.arguments.items():
-                if name not in hints:
-                    continue
+                for name, value in bound.arguments.items():
+                    if name not in hints:
+                        continue
 
-                hint = hints[name]
-                assert check_type(value, hint), (
-                    f"{name}={value!r} "
-                    f"is {type(value).__name__}, "
-                    f"expected {hint!r}"
-                )
+                    hint = hints[name]
+                    assert check_type(value, hint), (
+                        f"{name}={value!r} "
+                        f"is {type(value).__name__}, "
+                        f"expected {hint!r}"
+                    )
 
-            return super_init(*args, **kwargs)
+                return super_init(*args, **kwargs)
 
-        clsdict["__init__"] = new_init
-        return super().__new__(mcls, clsname, bases, clsdict)
+            ns["__init__"] = new_init
+        return super().__new__(mcls, name, bases, ns)
