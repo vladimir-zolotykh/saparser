c5bb7c34d4d3c352af9559a950408d96da63e4aa
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-07-21 09:24:33 +0300

    Refactor ValidateInitMeta with inject_assert
            if ns.get("_DO_INIT"):
                ns["__init__"] = mcls.inject_assert(ns["__init__"])

--- validatedinitmeta.py
+++ validatedinitmeta.py
@@ -15,29 +15,28 @@ def check_type(value: Any, hint: Any) -> bool:
 
 
 class ValidatedInitMeta(type):
+    @staticmethod
+    def inject_assert(super_init):
+        sig = inspect.signature(super_init)
+        hints = get_type_hints(super_init)
+
+        def init(*args, **kwargs):
+            bound = sig.bind(*args, **kwargs)
+            bound.apply_defaults()
+            for name, value in bound.arguments.items():
+                if name not in hints:
+                    continue
+                hint = hints[name]
+                assert check_type(value, hint), (
+                    f"{name}={value!r} "
+                    f"is {type(value).__name__}, "
+                    f"expected {hint!r}"
+                )
+            return super_init(*args, **kwargs)
+
+        return init
+
     def __new__(mcls, name, bases, ns):
         if ns.get("_DO_INIT"):
-            super_init = ns["__init__"]
-
-            sig = inspect.signature(super_init)
-            hints = get_type_hints(super_init)
-
-            def new_init(*args, **kwargs):
-                bound = sig.bind(*args, **kwargs)
-                bound.apply_defaults()
-
-                for name, value in bound.arguments.items():
-                    if name not in hints:
-                        continue
-
-                    hint = hints[name]
-                    assert check_type(value, hint), (
-                        f"{name}={value!r} "
-                        f"is {type(value).__name__}, "
-                        f"expected {hint!r}"
-                    )
-
-                return super_init(*args, **kwargs)
-
-            ns["__init__"] = new_init
+            ns["__init__"] = mcls.inject_assert(ns["__init__"])
         return super().__new__(mcls, name, bases, ns)
